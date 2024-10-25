d0bd5954537d05ce98542785d84df9e6965533da
Neil Mayhew <neil@mayhew.name>
2024-10-25 12:37:25 -0600

    Show optional timing information

--- Main.hs
+++ Main.hs
@@ -1,22 +1,29 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Main where
 
 import BreakWords
 
+import Control.DeepSeq
+import Control.Exception
 import Control.Monad
 import Data.Char
 import Data.Foldable
+import Data.Time
+import Data.Time.Clock.POSIX
 import Data.Tree
 import Data.Trie
 import Options.Applicative
+import System.IO
 
 import qualified System.Console.Terminal.Size as TS
 
 data Options = Options
   { optInputFiles :: [FilePath]
   , optWordListFile :: FilePath
+  , optVerbose :: Bool
   , optTree :: Bool
   , optDemo :: Bool
   }
@@ -37,6 +44,9 @@ main = do
             value "words.txt" <>
             help "A dictionary containing one word per line" <>
             showDefaultWith id
+          optVerbose <- switch $
+            short 'v' <> long "verbose" <>
+            help "Show extra information on stderr"
           optTree <- switch $
             long "tree" <>
             help "Display the parsing results as a tree"
@@ -57,6 +67,7 @@ main = do
     dict = fromList . (singles ++) $ filter (\w -> all isLower w && all isAlpha w && notSingle w) wordList
     singles = pure <$> "aio"
     notSingle = not . null . drop 1
+    verbose = when optVerbose . hPutStrLn stderr
 
     process input = do
       let
@@ -64,10 +75,16 @@ main = do
         parses = breakWords dict blob
         numParses = sum $ map treeSize parses
         paths = concatMap treePaths parses
-      putStrLn $ show numParses <> " parses"
-      if optTree
-        then putStr . drawTree $ Node "" parses
-        else mapM_ (putStrLn . unwords) paths
+      parsing <- timeEval numParses
+      verbose $ show numParses <> " parses in " <> show parsing
+      displaying <- timeExecute $
+        if optTree
+          then putStr . drawTree $ Node "" parses
+          else mapM_ (putStrLn . unwords) paths
+      verbose $ "Output produced in " <> show displaying
+
+  preparation <- timeEval dict
+  verbose $ "Dictionary built in " <> show preparation
 
   when optDemo $
     process "hand edit readability"
@@ -84,3 +101,15 @@ treePaths = foldTree go
 
 treeSize :: Tree String -> Int
 treeSize = foldTree $ \_ bs -> sum bs + fromEnum (null bs)
+
+timeEval :: NFData a => a -> IO NominalDiffTime
+timeEval = timeExecute . pure
+
+timeExecute :: NFData a => IO a -> IO NominalDiffTime
+timeExecute x = do
+  start <- getPOSIXTime
+  _ <- evaluate . force =<< x
+  end <- getPOSIXTime
+  return $ end - start
+
+instance NFData Trie
